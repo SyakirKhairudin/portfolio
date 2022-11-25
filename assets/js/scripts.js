e8fa01850ef0a83eddeadc9509226a26765d2f6c
SyakirKhairudin <kajirulmalbeh@gmail.com>
2022-11-25 18:23:30 +0800

    a

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,11 +2,3 @@ function myFunction(element,clr) {
     element.style.color = clr;
 }
 
-const colors = ["#ec4a57", "#d2d2d2"];
-
-function changeColor(element) {
-  var current = alphaButton.dataset.ci || 0;
-  current = (current + 1) % colors.length;
-  element.dataset.ci = current;
-  element.style.color = colors[current];
-}
