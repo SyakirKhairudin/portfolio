432526b66eac3c0c7909c422e20fa9df7852d0da
SyakirKhairudin <kajirulmalbeh@gmail.com>
2022-11-30 11:26:45 +0800

    ggez

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,28 @@
-function myFunction(element,clr) {
-    element.style.color = clr;
-}
 
-
-
-const colors = ["#d2d2d2", "#ec4a57"];
+const colorH = ["#d2d2d2", "#ec4a57"];
 
 function changeColorH(element) {
   var current = Number(element.dataset.ci || 0); // init to 0 on first click
-  current = (current + 1) % colors.length; // clamp array index
+  current = (current + 1) % colorH.length; // clamp array index
   element.dataset.ci = current; // store new value in element
-  element.style.color = colors[current]; // set color
+  element.style.color = colorH[current]; // set color
 }
 
-const color = ["#d2d2d2", "#1f41a2"];
+const colorB = ["#d2d2d2", "#1f41a2"];
 
 function changeColorB(element) {
   var current = Number(element.dataset.ci || 0); // init to 0 on first click
-  current = (current + 1) % color.length; // clamp array index
+  current = (current + 1) % colorB.length; // clamp array index
   element.dataset.ci = current; // store new value in element
-  element.style.color = color[current]; // set color
+  element.style.color = colorB[current]; // set color
+}
+
+
+const colorC = ["#7dd0af", "#dd576f", "#ffc166", "#a774be", "#566c85","#4a9bf3" ];
+
+function changeColorC(element) {
+  var current = Number(element.dataset.ci || 0); // init to 0 on first click
+  current = (current + 1) % colorC.length; // clamp array index
+  element.dataset.ci = current; // store new value in element
+  element.style.color = colorC[current]; // set color
 }
